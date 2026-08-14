5187bc6c86dddb4b6631b7f5cc85ed0678d45243
tarunr2799 <tarunr2799@users.noreply.github.com>
2026-08-14 11:07:11 +0530

    Set titles on review plugin pages

--- wp-content/plugins/science180-review-endorsements/includes/class-s180re-plugin.php
+++ wp-content/plugins/science180-review-endorsements/includes/class-s180re-plugin.php
@@ -862,6 +862,7 @@ class S180RE_Plugin
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo esc_html(wp_get_document_title()); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class('s180re-plugin-page'); ?>>
